c629dc523da3ecee4dd5048eb4b4f9e1b13fe601
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-08-15 11:01:28 -0500

    Complete Initial Model

---
+++ models/transaction.js
@@ -0,0 +1,46 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Transaction.belongsTo(models.User, { foreignKey: "userId" });
+      Transaction.belongsTo(models.CategoryId, { foreignKey: "categoryId" });
+    }
+  }
+  Transaction.init(
+    {
+      description: DataTypes.STRING,
+      amount: DataTypes.INTEGER,
+      date: DataTypes.DATE,
+      categoryId: {
+        type: DataTypes.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "categories",
+          key: "id"
+        }
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "users",
+          key: "id"
+        }
+      },
+      creditDebit: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: "Transaction",
+      tableName: "transactions"
+    }
+  );
+  return Transaction;
+};
