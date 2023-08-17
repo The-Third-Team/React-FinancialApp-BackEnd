270a01ab30c1ed1cd23accf8865bfce5bca7e643
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-08-17 00:20:54 -0500

    Complete seed files for user, category and budget

--- seeds/budgets.js
+++ seeds/budgets.js
@@ -15,21 +15,189 @@ module.exports = [
   },
   {
     name: "Coffee Shops",
-    budget: 1500,
+    budget: 5500,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Clothing",
+    budget: 0,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Jewelry",
+    budget: 6000,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Rent/Mortgage",
+    budget: 7000,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Car",
+    budget: 0,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Insurance",
+    budget: 2000,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Utilities",
+    budget: 3500,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Movies",
+    budget: 0,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Streaming",
+    budget: 1200,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Activities",
+    budget: 1800,
+    categoryId: "",
+    userId: "",
+    userEmail: "mary.hill@email.com"
+  },
+  {
+    name: "Restaurant",
+    budget: 1000,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Grocery",
+    budget: 800,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Coffee Shops",
+    budget: 5500,
     categoryId: "",
     userId: "",
     userEmail: "john.smith@email.com"
   },
   {
     name: "Clothing",
-    budget: 2000,
+    budget: 0,
     categoryId: "",
     userId: "",
-    userEmail: "alice.johnson@email.com"
+    userEmail: "john.smith@email.com"
   },
   {
     name: "Jewelry",
-    budget: 3000,
+    budget: 6000,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Rent/Mortgage",
+    budget: 7000,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Car",
+    budget: 0,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Insurance",
+    budget: 2000,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Utilities",
+    budget: 3500,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Movies",
+    budget: 0,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Streaming",
+    budget: 1200,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Activities",
+    budget: 1800,
+    categoryId: "",
+    userId: "",
+    userEmail: "john.smith@email.com"
+  },
+  {
+    name: "Restaurant",
+    budget: 2000,
+    categoryId: "",
+    userId: "",
+    userEmail: "michael.williams@email.com"
+  },
+  {
+    name: "Grocery",
+    budget: 1200,
+    categoryId: "",
+    userId: "",
+    userEmail: "michael.williams@email.com"
+  },
+  {
+    name: "Coffee Shops",
+    budget: 3500,
+    categoryId: "",
+    userId: "",
+    userEmail: "michael.williams@email.com"
+  },
+  {
+    name: "Clothing",
+    budget: 1800,
+    categoryId: "",
+    userId: "",
+    userEmail: "michael.williams@email.com"
+  },
+  {
+    name: "Jewelry",
+    budget: 0,
     categoryId: "",
     userId: "",
     userEmail: "michael.williams@email.com"
@@ -39,28 +207,28 @@ module.exports = [
     budget: 5000,
     categoryId: "",
     userId: "",
-    userEmail: "emily.brown@email.com"
+    userEmail: "michael.williams@email.com"
   },
   {
     name: "Car",
-    budget: 2500,
+    budget: 2800,
     categoryId: "",
     userId: "",
-    userEmail: "mary.hill@email.com"
+    userEmail: "michael.williams@email.com"
   },
   {
     name: "Insurance",
-    budget: 1200,
+    budget: 1500,
     categoryId: "",
     userId: "",
-    userEmail: "john.smith@email.com"
+    userEmail: "michael.williams@email.com"
   },
   {
     name: "Utilities",
-    budget: 1800,
+    budget: 3200,
     categoryId: "",
     userId: "",
-    userEmail: "alice.johnson@email.com"
+    userEmail: "michael.williams@email.com"
   },
   {
     name: "Movies",
@@ -71,37 +239,100 @@ module.exports = [
   },
   {
     name: "Streaming",
-    budget: 300,
+    budget: 900,
+    categoryId: "",
+    userId: "",
+    userEmail: "michael.williams@email.com"
+  },
+  {
+    name: "Activities",
+    budget: 1400,
+    categoryId: "",
+    userId: "",
+    userEmail: "michael.williams@email.com"
+  },
+  {
+    name: "Restaurant",
+    budget: 1800,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Grocery",
+    budget: 1500,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Coffee Shops",
+    budget: 4200,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Clothing",
+    budget: 2800,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Jewelry",
+    budget: 0,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Rent/Mortgage",
+    budget: 5500,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Car",
+    budget: 2500,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Insurance",
+    budget: 1300,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Utilities",
+    budget: 3000,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Movies",
+    budget: 800,
+    categoryId: "",
+    userId: "",
+    userEmail: "emily.brown@email.com"
+  },
+  {
+    name: "Streaming",
+    budget: 1000,
     categoryId: "",
     userId: "",
     userEmail: "emily.brown@email.com"
   },
   {
     name: "Activities",
-    budget: 900,
+    budget: 1600,
     categoryId: "",
     userId: "",
-    userEmail: "mary.hill@email.com"
-  },
-  {
-    name: "Restaurant",
-    budget: 1200,
-    categoryId: "",
-    userId: "",
-    userEmail: "john.smith@email.com"
-  },
-  {
-    name: "Grocery",
-    budget: 1000,
-    categoryId: "",
-    userId: "",
-    userEmail: "alice.johnson@email.com"
-  },
-  {
-    name: "Coffee Shops",
-    budget: 1800,
-    categoryId: "",
-    userId: "",
-    userEmail: "michael.williams@email.com"
+    userEmail: "emily.brown@email.com"
   }
 ];
