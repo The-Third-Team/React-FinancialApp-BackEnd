5092a33f353be619c6cbc74a31a1bba338d11f0f
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-08-21 14:46:41 -0500

    Complete initial prep for backend deployment

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    use_env_variable: "new_leaf_production",
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
